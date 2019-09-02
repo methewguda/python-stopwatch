a8ad49569ad3d0a33c842d28cad5618e50d148fb
Methew Guda <methewguda@gmail.com>
2019-09-02 00:16:08 -0700

    Added stopwatch script

---
+++ stopwatch.py
@@ -0,0 +1,27 @@
+import time
+
+# Display the program's instructions.
+print('Press ENTER to begin. Afterwards, '
+      'press ENTER to "click" the stopwatch. '
+      'Press Ctrl-C to quit.')
+
+input()
+print('Started.....')
+
+startTime = time.time()
+lastTime = startTime
+lapNum = 1
+
+# Start tracking lap time
+
+try:
+    while True:
+        input()
+        lapTime = round(time.time() - lastTime, 2)
+        totalTime = round(time.time() - startTime, 2)
+        print('Lap #%s: %s (%s)' % (lapNum, totalTime, lapTime), end='')
+        lapNum += 1
+        lastTime = time.time() # reset the last lap time
+except KeyboardInterrupt:
+    # Handle the Ctrl-C exception to keep its error message from displaying.
+    print('\nDone.')
